b35bd4e53b2c3791b148226d1024de5520d084a2
StringNick <wc3info@yandex.ru>
2016-05-21 23:59:10 +0300

    Тестирование free-kassa

--- app/Payment.php
+++ app/Payment.php
@@ -18,8 +18,8 @@ namespace App;
 class Payment {
     const CULTURE_EN = 'en';
     const CULTURE_RU = 'ru';
-    private $baseUrl      = 'https://merchant.roboxchange.com/Index.aspx?';
-    //private $baseUrl      = 'http://www.free-kassa.ru/merchant/cash.php?';
+    //private $baseUrl      = 'https://merchant.roboxchange.com/Index.aspx?';
+    private $baseUrl      = 'http://www.free-kassa.ru/merchant/cash.php?';
     private $isTestMode   = false;
     private $valid        = false;
     private $data;
