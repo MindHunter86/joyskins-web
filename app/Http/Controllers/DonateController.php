3e35525d7ca05c4b4a2b0a8bec34c20c488f85f5
StringNick <wc3info@yandex.ru>
2016-05-22 11:11:59 +0300

    free-kassa develop

--- app/Http/Controllers/DonateController.php
+++ app/Http/Controllers/DonateController.php
@@ -16,6 +16,7 @@ class DonateController extends Controller
 {
     public function payment(Request $request)
     {
+        dd($request);
         $payment = new Payment(
             Config::get('robokassa.login'), Config::get('robokassa.password1'), Config::get('robokassa.password2'), Config::get('robokassa.testmode')
         );
