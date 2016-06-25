d82911d2b50533c0275fc4790697c6cf163016b0
StringNick <wc3info@yandex.ru>
2016-06-25 16:18:26 +0300

    socket ip

--- public/assets/js/newapp.js
+++ public/assets/js/newapp.js
@@ -294,7 +294,7 @@ function replaceLogin(login) {
 }
 
 if (START) {
-    var socket = io.connect('51.254.222.131', { secure: true });
+    var socket = io.connect('51.254.222.131:5000', { secure: true });
     socket
         .on('connect', function () {
             $('#loader').hide();
