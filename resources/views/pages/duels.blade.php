60445c6bbf3a6eb84920d1b839efea1f13abc524
StringNick <wc3info@yandex.ru>
2016-07-01 22:22:50 +0300

    таблица инвентаря

--- resources/views/pages/duels.blade.php
+++ resources/views/pages/duels.blade.php
@@ -9,17 +9,29 @@
         .inv_choosen {
             background-color: greenyellow;
         }
+        .btnCreateRoom {
+            display: inline-block;
+            text-align: right;
+            float: right;
+            padding: 15px;
+            background-color: yellowgreen;
+            text-decoration: none;
+        }
     </style>
     <div class="content_bg">
         <div class="full">
             <div class="content_title"><div>Ваш <b>ИНВЕНТАРЬ.</b>Выберите ставку.</div></div>
             <div class="clear"></div>
             <div class="inv_cash">Загрузка инвентаря...</div>
-            <div class="inv_info">Предметов выбрано: <span class="inv_count">0</span>, Сумма предметов: <span class="inv_price">0</span></div>
-            <h2>Ваш инвентарь:</h2>
+            <div style="display: block;">
+                <div class="inv_info content_title">Предметов выбрано: <span class="inv_count">0</span>, Сумма предметов: <span class="inv_price">0</span></div>
+                <h2 style="display:inline-block; float: left;">Ваш инвентарь:</h2>
+                <a class="btnCreateRoom">Создать комнату</a>
+            </div>
             <div class="inv_table_duel">
 
             </div>
+
         </div>
     </div>
     <script>
