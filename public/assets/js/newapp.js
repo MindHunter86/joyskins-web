475c3127850068f09657f173a4829019e06c2895
StringNick <wc3info@yandex.ru>
2016-07-01 21:50:33 +0300

    таблица инвентаря

--- public/assets/js/newapp.js
+++ public/assets/js/newapp.js
@@ -575,11 +575,11 @@ function loadMyDuelInventory() {
             if (data.success && data.rgInventory && data.rgDescriptions) {
                 text = '';
                 var items = mergeWithDescriptions(data.rgInventory, data.rgDescriptions);
-                console.table(items);
+                //console.table(items);
                 //items.sort(function(a, b) { return parseFloat(b.price) - parseFloat(a.price) });
                 _.each(items, function(item) {
                     item.price = parseFloat(item.price);
-                    if(item.price < 5 || !item.tradable) return;
+                    if(item.price < 5 || !parseInt(item.tradable)) return;
                     item.image = 'https://steamcommunity-a.akamaihd.net/economy/image/class/730/'+item.classid+'/200fx200f';
                     item.market_name = item.market_name || '';
                     text += '<div class="inv_d_item"><img width="50" height="50" src="'+item.image+'"></div>';
