9b37f2b223b1c7f435b7589e26013a9b2f4ec71b
StringNick <wc3info@yandex.ru>
2016-07-01 21:42:18 +0300

    таблица инвентаря

--- resources/views/pages/duels.blade.php
+++ resources/views/pages/duels.blade.php
@@ -1,6 +1,20 @@
 @extends('layout')
 
 @section('content')
+    <div class="content_bg">
+        <div class="full">
+            <div class="content_title"><div>Ваш <b>ИНВЕНТАРЬ</b></div></div>
+            <div class="clear"></div>
+            <div class="inv_cash">Загрузка инвентаря...</div>
+            <div class="inv_table">
+                <div class="inv_table_panel">
+                    <div class="type1">Название</div>
+                    <div class="type2">Качество</div>
+                    <div class="type3">Цена</div>
+                </div>
+            </div>
+        </div>
+    </div>
     <script>
         $(function(){
             $.ajaxSetup({
@@ -11,6 +25,4 @@
             loadMyDuelInventory();
         });
     </script>
-    <div class="inv-cash"></div>
-    <div class="inv-table"></div>
 @endsection
