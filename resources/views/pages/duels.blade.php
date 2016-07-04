8d86beb465b67436dd0227fb473fdd02fcb8f767
StringNick <wc3info@yandex.ru>
2016-07-05 00:35:10 +0300

    дуэли миграции, модели

--- resources/views/pages/duels.blade.php
+++ resources/views/pages/duels.blade.php
@@ -171,7 +171,7 @@
     </style>
     <div class="content_bg">
         <div class="full">
-            @foreach(\App\duel_bet::where('status',\App\duel_bet::STATUS_WAIT_TO_ACCEPT)->where('user_id',\Auth::user()->id) as $bet)
+            @foreach(\App\duel_bet::where('status',\App\duel_bet::STATUS_WAIT_TO_ACCEPT)->where('user_id',\Auth::user()->id)->get() as $bet)
                 <h3>Вы вступили в игру, у вас есть: 60 сек для подтверждения</h3>
                 <h3>Ваша ставка:</h3>
                 <?php $items = json_decode($bet->items);?>
