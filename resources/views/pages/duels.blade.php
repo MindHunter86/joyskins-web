657bff216d8e14fa80854fb4cb4df284fbcf8686
StringNick <wc3info@yandex.ru>
2016-07-06 01:53:15 +0300

    дуэли миграции, модели

--- resources/views/pages/duels.blade.php
+++ resources/views/pages/duels.blade.php
@@ -239,6 +239,7 @@
                          $duel_bet = \App\duel_bet::where('game_id',$duel['id'])->where('status',\App\duel_bet::STATUS_ACCEPTED)->first();
                         //$items = json_decode($duel_bet->items);
                         ?>
+
                         <tr data-amount="2.85" data-id="5776e133ec1914830cb7a4e0" style="display: table-row;">
                             <td class="cf-players">
                                 @if($duel_bet->coin == 1)
