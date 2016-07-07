dab53458f63707d8de5a6be91849a88f09a0674d
StringNick <wc3info@yandex.ru>
2016-07-08 01:27:52 +0300

    response fix

--- resources/views/includes/room.blade.php
+++ resources/views/includes/room.blade.php
@@ -46,8 +46,7 @@ if(count($duel_bets)>1)
                 var time = {{$diff}};
                 setInterval(function () {
                     if(time<=0) return;
-                    console.log(parseInt(time*100/90));
-                    timer.radialProgress("to", {'perc': parseInt(time*100/90) , 'time': 1});
+                    timer.radialProgress("to", {'perc': parseInt(time*100/90) , 'time': parseInt(time*100/90) ? 100 : 10});
                     time--;
                 },1000);
             </script>
