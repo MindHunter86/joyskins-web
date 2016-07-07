5abafb7b0fbd327f3e3e8b2f6e0cba379ad26565
StringNick <wc3info@yandex.ru>
2016-07-08 01:14:48 +0300

    response fix

--- resources/views/includes/room.blade.php
+++ resources/views/includes/room.blade.php
@@ -46,7 +46,7 @@ if(count($duel_bets)>1)
                 var time = {{$diff}}
                 var timeout = setTimeout(function () {
                     time--;
-                    timer.radialProgress("to", {'perc': time, 'time': 10000});
+                    timer.radialProgress("to", {'perc': time, 'time': 1});
                     if(time <= 0)
                             return;
                             timeout();
