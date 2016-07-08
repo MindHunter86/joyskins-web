92e1532b81a6bb51ab398cd0019e650d3420681b
StringNick <wc3info@yandex.ru>
2016-07-09 00:40:55 +0300

    response fix

--- resources/views/pages/duels.blade.php
+++ resources/views/pages/duels.blade.php
@@ -308,7 +308,6 @@
             box-shadow: 1px 1px 3px #000;
         }
         .closeView:hover { background: #990000; }
-        .item{background: #700005;}
     </style>
     <div class="content_bg">
         <div class="full">
