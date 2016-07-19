ee4db03a19638e9f7c6ac6848efc39985692197c
StringNick <wc3info@yandex.ru>
2016-07-19 16:55:04 +0300

    timeout show winner

--- resources/views/pages/duels.blade.php
+++ resources/views/pages/duels.blade.php
@@ -414,9 +414,7 @@
         }
     </style>
     <div class="content_bg">
-        <script> $( function() {
-                $( "#dialog" ).dialog();
-            } );</script>
+
         <div id="dialog" title="Basic dialog">
             <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
         </div>
