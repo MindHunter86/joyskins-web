0b49baa6f2019264221267330e1c6a8601cc35e7
StringNick <wc3info@yandex.ru>
2016-07-21 18:04:30 +0300

    modals section

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -138,44 +138,7 @@
     <div class="footer"><a href="https://vk.com/joyskins_top">МЫ ВКонтакте</a></div>
 
     <div class="none">
-        <div class="box-modal" id="aboutDuel" style="width:900px;">
-            <div class="box-modal-top"><div class="box-modal_close arcticmodal-close"></div>О сайте</div>
-            <div class="rules">
-                <div style="margin-bottom: 10px; border-left: 1px solid #FFBD4C; padding-left: 6px;">
-                    <span style="color: #FFBD57;">О Дуэлях</span>-Вы можете создать новую дуэль или присоединиться к уже существующей. Вам необходимо выбрать вещи из инвентаря на нашей странице, а затем нажмите кнопку, чтобы создать. После этого мы вышлем Вам трейд оффер в кратчайшие сроки,который содержит элементы, которые вы выбрали. Вы должны принять это предложение и подтвердить его с помощью мобильного телефона. После того как вы подтверждаете,у вас создатся ваша комната. <br>
-                    Победитель определяется случайным образом, шанс выигрыша зависит от стоимости внесенных скинов.
-                </div>
-                <div style="margin-bottom: 10px; padding: 5px 6px; border: 1px solid #5cb85c;">
-                    <span style="text-transform: uppercase; color: #3FAA5D;">ПОБЕДИТЕЛЬ ПРЕДМЕТОВ:</span>По состоянию конца игры, победитель будет показан. Победитель получит все элементы которые были в раунде. В каждой игре мы забираем 0-15%. Он рассчитывается от общей стоимости банка.</div>
-                <div style="margin-bottom: 10px; border-left: 1px solid #60B3E5; padding-left: 6px; line-height: 16px;">
-                    <span style="color: #60B3E5; padding-bottom: 5px; text-transform: uppercase;">ПРОСОЕДИНИТСЯ К ИГРЕ:</span><br>
-                    <div style="margin-bottom: 8px; margin-top: 2px; padding-left: 30px;">
-                        1. <span style="">Вы можете присоединиться к раунду, нажав на кнопку войти. После присоединения у Вас будет 90 секунд на принятие обмена. Если время истекло, мы отменим отправленное предложение и комната снова будет свободна.
-Для того, чтобы создать комнату, вы должны иметь Steam, включен Mobile Authenticator.
 
-<br></span>
-                    </div>
-
-
-                </div>
-                <div class="rules_text" style="margin-bottom: 10px;padding-left: 6px;line-height: 18px;border: 1px solid #3FAA80;">
-                    <div style="color: #EC785D; padding-top: 5px; text-transform: uppercase;">Правила и особенности:</div>
-                    <ol style="padding: 0px 30px; margin: 3px; line-height: 15px; font-size: 13px;">
-                        <li style="padding-bottom: 6px;">Максимальный депозит - 15 предметов на трейд. Нет ограничений по стоимости предметов. Стоимость одного депозита - минимум 30р.</li>
-                        <li style="padding-bottom: 6px;">Для развития сайта и проведения конкурсов, мы взымаем комиссию с каждой игры - до 10% от всех вещей игры.</li>
-                        <li style="padding-bottom: 6px;">Депозиты и вывод призового фонда происходят автоматически. Срок отправки выигрыша зависит от загруженности ботов и серверов Steam (в среднем - 1-5 минут)</li>
-                        <li style="padding-bottom: 6px;">Каждый раз отправляя предметы, Вы соглашаетесь с правилами использования сайта.</li>
-                        <li style="padding-bottom: 6px;">Если Ваш инвентарь закрыт, и\или обмены разрешены только с друзьями, приз будет аннулирован!</li>
-                        <li style="padding-bottom: 6px;">Принимаются вещи только из CS:GO, другие вещи будут приняты, но не засчитаны на сайте. Так-же мы можем гарантировать правильную оценку стоимости вещи только тогда, когда она есть на Торговой площадке Steam, иначе ваш предмет может быть неверно оценен.</li>
-                        <li style="padding-bottom: 6px;">Вы имеете гарантию получения ваших вещей в течение получаса с момента закрытия пула. По истечении этого времени мы не несем ответственности за утерянные вещи.</li>
-                        <li style="padding-bottom: 6px;">Если вы отменили обмен или отправили контр-предложение после победы, то ваши вещи возвращены вам не будут, так как бот не рассчитан на повторную отправку вещей</li>
-                        <li style="padding-bottom: 6px;">Если нашего бота забанили в течение 30 минут с окончания матча, мы возмещаем только вашу ставку, но не выигрыш.</li>
-                        <li style="padding-bottom: 6px;"></li>
-                    </ol>
-                </div>
-                <a href="http://www.free-kassa.ru/"><img src="./JOYSKINS.TOP_files/13.png"></a>
-            </div>
-        </div>
         <div class="box-modal" id="about" style="width:900px;">
             <div class="box-modal-top"><div class="box-modal_close arcticmodal-close"></div>О сайте</div>
             <div class="rules">
