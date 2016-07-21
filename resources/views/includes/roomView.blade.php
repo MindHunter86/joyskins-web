d2f28494f15fe14ceb44e69bd03fca923524cbe9
StringNick <wc3info@yandex.ru>
2016-07-22 02:49:16 +0300

    hot fix spin

--- resources/views/includes/roomView.blade.php
+++ resources/views/includes/roomView.blade.php
@@ -120,7 +120,7 @@ background-color: #236235;">CoinFlip # {{$duel->id}}</h1>
 
         @if($duel->status == \App\duel::STATUS_FINISHED)
             <script>setTimeout(function(){$('#winner_info').show();},3000);</script>
-            <p id="winner_info" style="display:none; text-align: center;">
+            <p id="winner_info" style="display:none; text-align: center; padding-right: 5.5%;">
                 Процент раунда: {{$duel->rand_number*100}}%<br>Секретное слово: {{$duel->secret}}<br>Банк : {{$total_bet}}
             </p>
         @endif
