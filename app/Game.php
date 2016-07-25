2cd27b7e6513cfd6eb1f83c4444a1363d4c79f13
StringNick <wc3info@yandex.ru>
2016-07-25 16:10:39 +0300

    history

--- app/Game.php
+++ app/Game.php
@@ -27,7 +27,7 @@ class Game extends Model
         $key = md5('history_game_'.$id);
         if(!\Cache::has($key)) {
             $game = self::where('id',$id)->with(['bets','winner'])->get();
-            \Cache::put($key,json_decode($game),60);
+            \Cache::put($key,json_encode($game),60);
         }
         return json_decode(\Cache::get($key));
     }
