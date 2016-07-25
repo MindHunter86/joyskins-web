75a0caae9fcd303a9471c48dc85ea44ef81c79ae
StringNick <wc3info@yandex.ru>
2016-07-25 15:38:07 +0300

    my_history

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -46,7 +46,7 @@ class AjaxController extends Controller
         $html = '';
         foreach($gamesId as $duelId)
         {
-            $duel = \App\duel::get_history_duel($duelId->id);
+            $duel = \App\duel::get_history_duel($duelId['id']);
             $html .= view('includes.room', compact('duel'))->render();
         }
         return response($html);
