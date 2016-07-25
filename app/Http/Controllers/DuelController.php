89a0fdd922bfd83a03446852cd9e03de6da21441
StringNick <wc3info@yandex.ru>
2016-07-25 18:31:05 +0300

    history

--- app/Http/Controllers/DuelController.php
+++ app/Http/Controllers/DuelController.php
@@ -110,7 +110,7 @@ class DuelController extends Controller
         $comission_price = $duel->price*2*0.1; // предположительная комиссия
         $sendItems = []; // предметы выигрыша
         $tempPrice = 0;  // сколько взяли комиссия
-        foreach($items as $item) {
+        foreach($items as &$item) {
             $item['price'] = CsgoFast::getPriceFromCache($item['market_hash_name']);
             if( $item['price'] )
                 $room_price += $item['price'];
